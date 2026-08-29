50c9874bad902a7de6adca68699db8601ac66b3a
sys-Ninja <var69801@gmail.com>
2026-08-29 20:18:19 +0300

    fix(build): add gradle.properties with android.useAndroidX=true for screentranslator and ghostengine

--- unica/apps/ghostengine/app/build.gradle.kts
+++ unica/apps/ghostengine/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.mesalabs.unica.ghostengine"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.mesalabs.unica.ghostengine"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
--- unica/apps/screentranslator/app/build.gradle.kts
+++ unica/apps/screentranslator/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.mesalabs.unica.screentranslator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.mesalabs.unica.screentranslator"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
